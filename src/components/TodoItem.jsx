4f101ffcaef778666e4e0bfff0451aae51b5b6b0
edwinwalela <edwinwalela@gmail.com>
2019-07-12 22:09:21 +0300

    dynamic styles

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,18 +4,28 @@ import PropTypes from 'prop-types';
 import '../App.css';
 
 class TodoItem extends Component{
+
+    getStyle = (todo) => {
+        return {
+            margin:'auto',
+            padding:'15px',
+            textDecoration: todo.completed ? 'line-through' : 'none',
+            background: '#f4f4f4',
+            borderBottom:'1px #ccc dotted'
+        }
+    }
+
     render(){
         let todo = this.props.todo;
         return(
-            <div>
-                <p>{todo.id}</p>
+            <div style={this.getStyle(todo)}>
                 <p>{todo.title}</p>
-                <p>{todo.completed}</p>
             </div>
         )
     }
 }
 
+// PropTypes
 TodoItem.propTypes = {
     todo:PropTypes.object.isRequired
 }
